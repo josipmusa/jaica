ceecde9246182d7334a241aff380218dfc3a95d4
josipmusa <josip.musa42@gmail.com>
2025-11-23 23:05:12 +0100

    Change demo model url to use onnx

--- src/app/models/code_classifier/demo.py
+++ src/app/models/code_classifier/demo.py
@@ -1,6 +1,6 @@
 from src.app.models.code_classifier.code_classifier import CodeClassifier
 
-model_url = "https://huggingface.co/josipmusa/code-classifier/resolve/main/code_classifier_traced.pt"
+model_url = "https://huggingface.co/josipmusa/code-classifier/resolve/main/code_classifier.onnx"
 label_url = "https://huggingface.co/josipmusa/code-classifier/resolve/main/labels.json"
 clf = CodeClassifier(model_url, label_url)
 example = "private final MyService myService;"
