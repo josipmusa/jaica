4c789e1b6dd24c216f7d41857c6b27f5985a0cfb
josipmusa <josip.musa42@gmail.com>
2025-12-05 19:08:31 +0100

    Remove bug classifier, model too large

--- src/app/configuration/config.py
+++ src/app/configuration/config.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-BUG_CLASSIFIER_MODEL_URL = "https://huggingface.co/josipmusa/bug-classifier/resolve/main/bug_classifier.onnx"
-BUG_CLASSIFIER_LABEL_URL = "https://huggingface.co/josipmusa/bug-classifier/resolve/main/labels.json"
 CODE_CLASSIFIER_MODEL_URL = "https://huggingface.co/josipmusa/code-classifier/resolve/main/code_classifier.onnx"
 CODE_CLASSIFIER_LABEL_URL = "https://huggingface.co/josipmusa/code-classifier/resolve/main/labels.json"
 PROJECT_ROOT = Path(__file__).parent.parent.parent.parent
--- src/app/configuration/dependencies.py
+++ src/app/configuration/dependencies.py
@@ -1,22 +1,17 @@
 from src.app.configuration import config
 from src.app.configuration.db import VectorDB
 from src.app.models.code_classifier.code_classifier import CodeClassifier
-from src.app.models.bug_classifier.bug_classifier import BugClassifier
 from src.app.services.code_analysis_service import CodeAnalyzer
 from src.app.services.rag_service import RagService
 
 code_classifier_instance = CodeClassifier(config.CODE_CLASSIFIER_MODEL_URL, config.CODE_CLASSIFIER_LABEL_URL)
-bug_classifier_instance = BugClassifier(config.BUG_CLASSIFIER_MODEL_URL, config.BUG_CLASSIFIER_LABEL_URL)
 db_instance = VectorDB(persist_dir=str(config.VECTORSTORE_PATH))
 rag_service_instance = RagService(db=db_instance)
-code_analyzer_instance = CodeAnalyzer(bug_classifier=bug_classifier_instance, code_classifier=code_classifier_instance)
+code_analyzer_instance = CodeAnalyzer(code_classifier=code_classifier_instance)
 
 
 def get_code_classifier() -> CodeClassifier:
     return code_classifier_instance
-
-def get_bug_classifier() -> BugClassifier:
-    return bug_classifier_instance
 
 def get_db() -> VectorDB:
     return db_instance
